57a08a1c73db758780205f30ccb4076be6c7717e
Ryan Hauk <rhauk@akamai.com>
2025-09-22 23:35:27 -0500

    Added main.tf to define data sources

---
+++ main.tf
@@ -0,0 +1,21 @@
+data "akamai_group" "my_group_id" {
+  group_name = "Script Club"
+  contract_id = "ctr_1-5C13O2"
+}
+
+output "my_group_id" {
+  value = data.akamai_group.my_group_id
+}
+
+data "akamai_property" "my_property" {
+  name        = "js-scriptclub"
+}
+
+output "my_property" {
+  value = data.akamai_property.my_property
+}
+
+data "akamai_appsec_configuration" "my_appsec_config" {
+    name = "csgt"
+}
+
